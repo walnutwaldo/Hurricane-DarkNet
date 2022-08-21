9730f4516b145392b6a157537cd65931a5fc7865
Walden Yan <waldenyan20@gmail.com>
2022-08-21 14:37:48 -0700

    refactor: moved from js to ts

---
+++ frontend/zk-nfts/src/App.tsx
@@ -0,0 +1,36 @@
+import './styles/App.css';
+import Content from "./Content";
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <div className={"bg-cyan-900"}>
+                <div className="container mx-auto py-2">
+                    <h1 className="text-xl text-white font-bold">
+                        zkNFTs
+                    </h1>
+                </div>
+            </div>
+            <div className={"container mx-auto py-4"}>
+                <Content/>
+            </div>
+            <div className={"bg-cyan-900"}>
+                <div className="container mx-auto py-2">
+                    <div className="flex flex-row justify-between text-white">
+                        <h1 className="text-md">
+                            Built at <b>Hacklodge 2020</b>
+                        </h1>
+                        <p className={"text-right"}>
+                            Walden Yan<br/>
+                            Allison Chen <br/>
+                            Brandon Wang
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
