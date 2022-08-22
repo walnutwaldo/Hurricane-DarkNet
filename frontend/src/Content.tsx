87370c5eb08891050eaae6a5b88de5b2ab00bfdb
Walden Yan <waldenyan20@gmail.com>
2022-08-21 21:06:42 -0700

    feat: generate multiplication proof

--- frontend/src/Content.tsx
+++ frontend/src/Content.tsx
@@ -1,33 +1,71 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 // @ts-ignore
 const {groth16} = snarkjs;
 
 export default function Content() {
+    const [error, setError] = useState("");
+    const [proof, setProof] = useState(undefined);
+    const [publicSignals, setPublicSignals] = useState(undefined);
 
-    async function runProof() {
+    const aRef = useRef<HTMLInputElement>(null);
+    const bRef = useRef<HTMLInputElement>(null);
+
+    async function runProof(a: number, b: number) {
         console.log("starting proof");
         const {proof, publicSignals} = await groth16.fullProve({
-            a: 10,
-            b: 21
+            a: a,
+            b: b
         }, "circuit/multiply.wasm", "circuit/multiply.zkey");
-        console.log("Proof", proof);
-        console.log("Public Signals", publicSignals);
+        setProof(proof);
+        setPublicSignals(publicSignals)
     }
 
     return (
         <div>
-            <form>
-                <label>A:</label>
-                <input type="text" name="a" className={"ml-1 rounded-md outline-none bg-slate-100 px-1 mb-1"}/><br/>
-                <label>B:</label>
-                <input type="text" name="b" className={"ml-1 rounded-md outline-none bg-slate-100 px-1 mb-1"}/><br/>
-                <button onClick={async (e) => {
+            <form onSubmit={
+                async (e) => {
                     e.preventDefault();
-                    return runProof();
-                }} className={"bg-slate-200 p-1 rounded-md"}>Run Proof
+                    // Get fields A and B as numbers
+                    const a = parseInt(aRef.current!.value);
+                    const b = parseInt(bRef.current!.value);
+                    if (Number.isNaN(a) || Number.isNaN(b)) {
+                        setError("Please enter valid numbers");
+                    } else {
+                        setError("");
+                    }
+                    console.log("A:", a);
+                    console.log("B:", b);
+                    return runProof(a, b);
+                }
+            }>
+                <label>A:</label>
+                <input type="text" name="a" ref={aRef}
+                       className={"ml-1 rounded-md outline-none bg-slate-100 px-1 mb-1"}/><br/>
+                <label>B:</label>
+                <input type="text" name="b" ref={bRef}
+                       className={"ml-1 rounded-md outline-none bg-slate-100 px-1 mb-1"}/><br/>
+                <button type="submit" className={"bg-slate-200 p-1 rounded-md"}>
+                    Generate Proof
                 </button>
             </form>
+            <div className={"text-red-500"}>{error}</div>
+            {proof && (
+                <div>
+                    <h1>Proof</h1>
+                    <div className={"bg-slate-100 p-1 rounded-md whitespace-pre-wrap text-sm font-mono"}>
+                        {JSON.stringify(proof, null, '\t')}
+                    </div>
+                </div>
+            )}
+            {publicSignals && (
+                <div>
+                    <h1>Public Signals</h1>
+                    <div className={"bg-slate-100 p-1 rounded-md whitespace-pre-wrap text-sm font-mono"}>
+                        {JSON.stringify(publicSignals, null, '\t')}
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
