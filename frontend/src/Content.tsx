d8a11bca0aa6b42d6aec4e09a4b54ade9dc7af5d
Walden Yan <waldenyan20@gmail.com>
2022-08-22 13:57:04 -0700

    feat: secret number gen

--- frontend/src/Content.tsx
+++ frontend/src/Content.tsx
@@ -14,142 +14,87 @@ const {groth16, zKey} = snarkjs;
 
 const VERIFIER_CONTRACT_ADDRESS = "0x05298fbF8C22BF842ce711bA5A018B29d6caF65a";
 
+const MODULUS = BigNumber.from(2).pow(256).sub(BigNumber.from(2).pow(32)).sub(977)
+
 export default function Content() {
-    const [proofError, setProofError] = useState("");
-    const [proof, setProof] = useState<{
-        pi_a: [string, string],
-        pi_b: [[string, string], [string, string]],
-        pi_c: [string, string],
-    } | undefined>(undefined);
-    const [
-        publicSignals,
-        setPublicSignals
-    ] = useState<string[] | undefined>(undefined);
+    // const [proofError, setProofError] = useState("");
+    // const [proof, setProof] = useState<{
+    //     pi_a: [string, string],
+    //     pi_b: [[string, string], [string, string]],
+    //     pi_c: [string, string],
+    // } | undefined>(undefined);
+    // const [
+    //     publicSignals,
+    //     setPublicSignals
+    // ] = useState<string[] | undefined>(undefined);
 
-    const aRef = useRef<HTMLInputElement>(null);
-    const bRef = useRef<HTMLInputElement>(null);
-    const cRef = useRef<HTMLInputElement>(null);
+    // const args = (proof && publicSignals) ? [
+    //     [BigNumber.from(proof.pi_a[0]), BigNumber.from(proof.pi_a[1])],
+    //     [
+    //         [BigNumber.from(proof.pi_b[0][1]), BigNumber.from(proof.pi_b[0][0])],
+    //         [BigNumber.from(proof.pi_b[1][1]), BigNumber.from(proof.pi_b[1][0])]
+    //     ],
+    //     [BigNumber.from(proof.pi_c[0]), BigNumber.from(proof.pi_c[1])],
+    //     publicSignals.map((s) => BigNumber.from(s)),
+    // ] : [];
+    //
+    // async function runProof(a: number, b: number, c: number) {
+    //     console.log("starting proof");
+    //     const {proof, publicSignals} = await groth16.fullProve({
+    //         a: a,
+    //         b: b,
+    //         c: c
+    //     }, "circuit/multiply.wasm", "circuit/multiply.zkey");
+    //     setProof(proof);
+    //     setPublicSignals(publicSignals)
+    // }
 
-    const args = (proof && publicSignals) ? [
-        [BigNumber.from(proof.pi_a[0]), BigNumber.from(proof.pi_a[1])],
-        [
-            [BigNumber.from(proof.pi_b[0][1]), BigNumber.from(proof.pi_b[0][0])],
-            [BigNumber.from(proof.pi_b[1][1]), BigNumber.from(proof.pi_b[1][0])]
-        ],
-        [BigNumber.from(proof.pi_c[0]), BigNumber.from(proof.pi_c[1])],
-        publicSignals.map((s) => BigNumber.from(s)),
-    ] : [];
+    // const {data, error, isError, isLoading} = useContractRead({
+    //     addressOrName: VERIFIER_CONTRACT_ADDRESS,
+    //     contractInterface: VERIFIER_CONTRACT_ABI,
+    //     functionName: 'verifyProof',
+    //     args: args,
+    // });
 
-    async function runProof(a: number, b: number, c: number) {
-        console.log("starting proof");
-        const {proof, publicSignals} = await groth16.fullProve({
-            a: a,
-            b: b,
-            c: c
-        }, "circuit/multiply.wasm", "circuit/multiply.zkey");
-        // console.log(await groth16.exportSolidityCallData(publicSignals, proof));
-        setProof(proof);
-        setPublicSignals(publicSignals)
-    }
-
-    const {data, error, isError, isLoading} = useContractRead({
-        addressOrName: VERIFIER_CONTRACT_ADDRESS,
-        contractInterface: VERIFIER_CONTRACT_ABI,
-        functionName: 'verifyProof',
-        args: args,
-    });
-
-    async function verifyProof() {
-        // console.log("data", data);
-        // const txnResponse = await data.wait();
-        // console.log("txnResponse", txnResponse);
-    }
-
-    const ProofSection = (<div>
-        <h2 className={"font-bold text-xl mb-2"}>Prover</h2>
-        <form onSubmit={
-            async (e) => {
-                e.preventDefault();
-                // Get fields A and B as numbers
-                const a = parseInt(aRef.current!.value);
-                const b = parseInt(bRef.current!.value);
-                const c = parseInt(cRef.current!.value);
-                if (Number.isNaN(a) || Number.isNaN(b) || Number.isNaN(c)) {
-                    setProofError("Please enter valid numbers");
-                } else {
-                    setProofError("");
-                    return runProof(a, b, c);
-                }
-            }
-        }>
-            <label>A:</label>
-            <input type="text" name="a" ref={aRef}
-                   className={"ml-1 rounded-md outline-none bg-slate-100 px-1 mb-1"}/><br/>
-            <label>B:</label>
-            <input type="text" name="b" ref={bRef}
-                   className={"ml-1 rounded-md outline-none bg-slate-100 px-1 mb-1"}/><br/>
-            <label>C:</label>
-            <input type="text" name="c" ref={cRef}
-                   className={"ml-1 rounded-md outline-none bg-slate-100 px-1 mb-1"}/><br/>
-            <button type="submit" className={"bg-slate-200 p-1 rounded-md"}>
-                Generate Proof
-            </button>
-        </form>
-        <div className={"text-red-500"}>{proofError}</div>
-        {proof && (
-            <div>
-                <h1>Proof</h1>
-                <div className={"bg-slate-100 p-1 rounded-md whitespace-pre-wrap text-sm font-mono overflow-scroll"}>
-                    {JSON.stringify(proof, null, '\t')}
-                </div>
-            </div>
-        )}
-        {publicSignals && (
-            <div>
-                <h1>Public Signals</h1>
-                <div className={"bg-slate-100 p-1 rounded-md whitespace-pre-wrap text-sm font-mono overflow-scroll"}>
-                    {JSON.stringify(publicSignals, null, '\t')}
-                </div>
-            </div>
-        )}
-    </div>);
-
-    const VerifySection = (
-        <div>
-            <h2 className={"font-bold text-xl mb-2"}>Verifier</h2>
-            {
-                proof ? (
-                    <div>
-                        {/*<button className={"bg-slate-200 p-1 rounded-md"} onClick={verifyProof}>*/}
-                        {/*    Verify*/}
-                        {/*</button>*/}
-                        {isLoading && <div>Verifying proof ...</div>}
-                        {!isLoading && data && (
-                            <div>
-                                <h3 className="font-bold text-md">Valid:</h3>
-                                {JSON.stringify(data, null, '\t')}
-                            </div>
-                        )}
-                        {isError && (
-                            <div>
-                                <h3 className="font-bold text-md">Error:</h3>
-                                <div className={"bg-slate-100 p-1 rounded-md whitespace-pre-wrap text-sm font-mono overflow-scroll"}>
-                                    {JSON.stringify(error, null, '\t')}
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                ) : (
-                    <span>No proof to verify</span>
-                )
-            }
-        </div>
-    )
+    const [secret, setSecret] = useState<BigNumber | undefined>(undefined);
 
     return (
-        <div className={"grid grid-cols-2"}>
-            {ProofSection}
-            {VerifySection}
+        <div className={""}>
+            <h3 className={"text-lg text-black font-bold"}>
+                GENERATE A SECRET
+            </h3>
+            <div className={"flex flex-row gap-2"}>
+                <button className={
+                    "outline-none bg-teal-400 text-white rounded-md px-1 transition" +
+                    " hover:scale-105 font-bold"
+                } onClick={() => {
+                    const randomBytes = crypto.getRandomValues(new Uint32Array(10));
+                    // Concatenate into hex string
+                    const secretString = randomBytes.reduce((acc, cur) => acc + cur.toString(16), "");
+                    const secret = BigNumber.from("0x" + secretString).mod(MODULUS);
+                    setSecret(secret);
+                }}>
+                    Generate
+                </button>
+                {
+                    (secret !== undefined) && (
+                        <>
+                            <button className={
+                                "outline-none bg-zinc-400 text-white rounded-md px-1 transition" +
+                                " hover:scale-105 font-bold"
+                            } onClick={() => {
+                                // Copy secret.toHexString() to clipboard
+                                navigator.clipboard.writeText(secret.toHexString());
+                            }}>
+                                Copy
+                            </button>
+                            <span className={"px-1 bg-zinc-100 text-zinc-900 rounded-md"}>
+                            {secret.toHexString()}
+                        </span>
+                        </>
+                    )
+                }
+            </div>
         </div>
     );
 }
