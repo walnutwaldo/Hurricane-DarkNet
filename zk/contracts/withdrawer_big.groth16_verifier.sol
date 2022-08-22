2e9dd2e457812874151f98e6f4126bd27be00ab9
br-wa <brwa@mit.edu>
2022-08-22 19:24:01 -0400

    small and big versions of circuits and split pairing library

---
+++ zk/contracts/withdrawer_big.groth16_verifier.sol
@@ -0,0 +1,119 @@
+//
+// Copyright 2017 Christian Reitwiessner
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// 2019 OKIMS
+//      ported to solidity 0.6
+//      fixed linter warnings
+//      added requiere error messages
+//
+//
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.11;
+
+import "Pairing.sol";
+
+contract Verifier {
+    using Pairing for *;
+    struct VerifyingKey {
+        Pairing.G1Point alfa1;
+        Pairing.G2Point beta2;
+        Pairing.G2Point gamma2;
+        Pairing.G2Point delta2;
+        Pairing.G1Point[] IC;
+    }
+    struct Proof {
+        Pairing.G1Point A;
+        Pairing.G2Point B;
+        Pairing.G1Point C;
+    }
+    function verifyingKey() internal pure returns (VerifyingKey memory vk) {
+        vk.alfa1 = Pairing.G1Point(
+            9127932173196541174467917976808158259646361134300834064830538325546521338748,
+            4512056249911407189768052642758085772675923924305734758012231781083701262645
+        );
+
+        vk.beta2 = Pairing.G2Point(
+            [20937904441004326698748661328682908378768112852793673627720872927658021172288,
+             8914991964212291637697326483751152676113546092087092287523381508524247957614],
+            [13603215600384038804331591878364684174537303501557507750618559052198655051805,
+             16241025842602377393434594243896851212676443764213635547040836211839519970360]
+        );
+        vk.gamma2 = Pairing.G2Point(
+            [11559732032986387107991004021392285783925812861821192530917403151452391805634,
+             10857046999023057135944570762232829481370756359578518086990519993285655852781],
+            [4082367875863433681332203403145435568316851327593401208105741076214120093531,
+             8495653923123431417604973247489272438418190587263600148770280649306958101930]
+        );
+        vk.delta2 = Pairing.G2Point(
+            [10647439269202463052768414974225695181231269960826891681134998419163417853758,
+             15955613643013896649750755356874480475773273914904236608511744278155784563051],
+            [12471617669564647057697556646990218334797134347036466945875754171667891938611,
+             4401671436865411445147587928982453093467002530329794292817607615766310151325]
+        );
+        vk.IC = new Pairing.G1Point[](4);
+        
+        vk.IC[0] = Pairing.G1Point( 
+            14985800826283180184650528944214184508169368142923181197341625918212131977840,
+            7253830090219455600867850711936296873550090745716201562236783671022085460979
+        );                                      
+        
+        vk.IC[1] = Pairing.G1Point( 
+            5037058703238004567312606637624160788099099915847478853176536219825500895786,
+            4736108307932610274265303786589086091862657713204126904035464477664807283300
+        );                                      
+        
+        vk.IC[2] = Pairing.G1Point( 
+            18965822478706725895888882013956289865128605322296129845723534346822880025356,
+            17680819510396765959050392726942582041291855704862282678001869698209764853635
+        );                                      
+        
+        vk.IC[3] = Pairing.G1Point( 
+            6860980274063965046467763495713515083949295359678152760894728903202400019835,
+            18265438422188057466501471891732481863055917794973486114004917504582407070806
+        );                                      
+        
+    }
+    function verify(uint[] memory input, Proof memory proof) internal view returns (uint) {
+        uint256 snark_scalar_field = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+        VerifyingKey memory vk = verifyingKey();
+        require(input.length + 1 == vk.IC.length,"verifier-bad-input");
+        // Compute the linear combination vk_x
+        Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);
+        for (uint i = 0; i < input.length; i++) {
+            require(input[i] < snark_scalar_field,"verifier-gte-snark-scalar-field");
+            vk_x = Pairing.addition(vk_x, Pairing.scalar_mul(vk.IC[i + 1], input[i]));
+        }
+        vk_x = Pairing.addition(vk_x, vk.IC[0]);
+        if (!Pairing.pairingProd4(
+            Pairing.negate(proof.A), proof.B,
+            vk.alfa1, vk.beta2,
+            vk_x, vk.gamma2,
+            proof.C, vk.delta2
+        )) return 1;
+        return 0;
+    }
+    /// @return r  bool true if proof is valid
+    function verifyProof(
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[3] memory input
+        ) public view returns (bool r) {
+        Proof memory proof;
+        proof.A = Pairing.G1Point(a[0], a[1]);
+        proof.B = Pairing.G2Point([b[0][0], b[0][1]], [b[1][0], b[1][1]]);
+        proof.C = Pairing.G1Point(c[0], c[1]);
+        uint[] memory inputValues = new uint[](input.length);
+        for(uint i = 0; i < input.length; i++){
+            inputValues[i] = input[i];
+        }
+        if (verify(inputValues, proof) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
