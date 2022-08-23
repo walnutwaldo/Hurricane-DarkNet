f8fe722ad8cc730034bc4e2a2896f2fb0eb0eeaa
Allison Qi <aqi@princeton.edu>
2022-08-22 19:15:28 -0700

    t

--- zk/contracts/DB.sol
+++ zk/contracts/DB.sol
@@ -8,7 +8,7 @@ import "./verifier.groth16_verifier.sol";
 
 contract DB is ERC20, Verifier {
     uint256 size = 0;
-    uint256 constant HEIGHT = 4;
+    uint256 constant HEIGHT = 30;
     bytes32[][] merkleTree;
     bytes32 merkleRoot;
     mapping(bytes32 => bool) nullifiers;
@@ -52,20 +52,27 @@ contract DB is ERC20, Verifier {
     }
 
     function deposit(uint[2] memory a,
-            uint[2][2] memory b,
-            uint[2] memory c,
-            uint[93] memory input, uint256 memory index, uint256[HEIGHT] memory path) external payable noReentrant {
+    uint[2][2] memory b,
+    uint[2] memory c,
+    uint[93] memory input) external payable noReentrant {
         require(depositor.groth16_verifier(a, b, c, input), "Invalid Deposit Proof");
-        uint256 index = size;
         size++;
-        
+        uint256 index = size;
+        merkleRoot = bytes32(input[61]);
+        bytes32[31] path;
+        for (int i = 0; i<32; i++){
+            path[i] = bytes32(input[62+i]);
+        }
+
+        require((merkleRoot == path[HEIGHT]), "Merkle Root Invalid");
+
         for (uint i = 0; i<HEIGHT+1; i++){
             merkleTree[index][i] = path[i];
             index >>= 1;
         }
-        merkleRoot = path[HEIGHT];
-
+        
         emit Deposit(size);
+
         require(msg.value == 1 ether, "deposit must be 1 ether");
        //  IERC20(ETH_ADDRESS).transferFrom(msg.sender, address(this), 1);
     }
@@ -73,10 +80,13 @@ contract DB is ERC20, Verifier {
     function withdraw(uint[2] memory a,
             uint[2][2] memory b,
             uint[2] memory c,
-            uint[93] memory input, bytes32 memory merkleRoot, bytes32 memory nullifier) external noReentrant {
+            uint[3] memory input) external noReentrant {
+        bytes root = bytes32(input[1]);
+        bytes32 nullifer = bytes32(input[2]);
+
         require(nullifiers[nullifier]!= true, "Token Already Spent");
-        require(merkleRoot == merkleTree[0][merkleTree.length-1]);
-        require(verifier.groth16_verifier.verifyProof(a, b, c, input));
+        require((root == merkleTree[0][merkleTree.length-1]), "Merkle Root Invalid");
+        require(verifier.groth16_verifier.verifyProof(a, b, c, input), "Withdraw Proof Invalid");
         nullifers[nullifier]= true;
         emit Withdraw(nullifer);
         //IERC20(ETH_ADDRESS).transferFrom(address, msg.sender, address(this));
