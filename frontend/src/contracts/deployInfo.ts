481b52007b038fff212e662a8e7d1d5652fcd1ea
Walden Yan <waldenyan20@gmail.com>
2022-08-23 14:11:00 -0700

    feat: goerli contract deployed

--- frontend/src/contracts/deployInfo.ts
+++ frontend/src/contracts/deployInfo.ts
@@ -2,6 +2,6 @@ import abi from "../contracts/hurricane_abi.json";
 
 export const HURRICANE_CONTRACT_ABI = abi;
 export const HURRICANE_CONTRACT_ADDRESS = {
-    "goerli": "0x04E8195b4e03e55fE3244dD51652CE8E8f2945e4",
+    "goerli": "0x0B512bFA01843b22B8118E4Ce355935F8E5D0761",
     "localhost": "0x68B1D87F95878fE05B998F19b66F4baba5De1aed"
 };
