a960b41ab133584cd5cf402d2f68558c1b82f983
Walden Yan <waldenyan20@gmail.com>
2022-08-25 16:14:33 -0700

    feat: added NFT rescue

--- frontend/src/Content.tsx
+++ frontend/src/Content.tsx
@@ -1,13 +1,6 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {BigNumber, Contract} from "ethers";
-import SecretContext, {
-    jsonToSecret,
-    Secret,
-    secretToJson,
-    secretToPrivateKey,
-    secretToSharedKey, unmaskTokenData
-} from './contexts/SecretContext';
-import {AlertButton, SecondaryButton} from "./components/buttons";
+import React, {useContext, useState} from 'react';
+import {Contract} from "ethers";
+import SecretContext, {jsonToSecret, Secret, secretToJson} from './contexts/SecretContext';
 import {DepositSection} from "./sections/DepositSection";
 import {WithdrawSection} from "./sections/WithdrawSection";
 import {HURRICANE_CONTRACT_ABI, HURRICANE_CONTRACT_ADDRESSES} from "./contracts/deployInfo";
@@ -16,102 +9,7 @@ import {TransferSection} from "./sections/TransferSection";
 import {ConnectButton} from "@rainbow-me/rainbowkit";
 import NFTSection from "./sections/NFTSection";
 import {GenerateSecretSection} from "./sections/GenSecretSection";
-
-function SecretDisplay(props: {
-    secret: Secret,
-} & any) {
-
-    const {removeAsset, removeKey, updateStatus} = useContext(SecretContext);
-
-    const {secret, idx, upd, contract, isAsset} = props;
-
-    const [enableSecretCopy, setEnableSecretCopy] = useState(isAsset);
-    const [enableSharedCopy, setEnableSharedCopy] = useState(true);
-    const [enableIsPaidCopy, setEnableIsPaidCopy] = useState(true);
-    const [refreshing, setRefreshing] = useState(false);
-
-    async function refresh() {
-        if (!isAsset) return;
-        setRefreshing(true);
-        const leafIdx = BigNumber.from(await contract.leafForPubkey(secret.shared));
-        if (leafIdx.isZero()) {
-            setRefreshing(false);
-            return;
-        }
-
-        const maskedData = await contract.dataForPubkey(secret.shared);
-        const {tokenAddress, tokenId} = unmaskTokenData(maskedData, secret);
-        const leaf = await contract.calcLeaf(
-            secret.shared,
-            tokenAddress,
-            tokenId,
-            secret.noise
-        );
-        const isPaid = (await contract.getLeaf(leafIdx)).eq(leaf);
-        if (isPaid) {
-            updateStatus?.(idx);
-        }
-        setRefreshing(false);
-    }
-
-    useEffect(() => {
-        if (contract) {
-            refresh();
-        }
-    }, [])
-
-    return (
-        <div className={"flex flex-row gap-3 text-white"}>
-            <AlertButton onClick={() => {
-                {
-                    isAsset ? removeAsset!(idx) : removeKey!(idx)
-                }
-                ;
-            }}>
-                Delete
-            </AlertButton>
-            <label><b>Secret key:</b></label>
-            <SecondaryButton onClick={() => {
-                // Copy secret.toHexString() to clipboard
-                navigator.clipboard.writeText(secretToPrivateKey(secret));
-                setEnableSecretCopy(false);
-                setTimeout(() => {
-                    setEnableSecretCopy(true);
-                }, 1000);
-            }} disabled={!enableSecretCopy}>
-                {enableSecretCopy || !isAsset ? "Copy" : "Copied!"}
-            </SecondaryButton>
-            <span className={"px-1 bg-zinc-400 text-zinc-900 rounded-md font-mono"}>
-                {secretToPrivateKey(secret).substr(0, 6) + "..."}
-            </span>
-            {
-                isAsset ? <></> : <>
-                    <label><b>Shared key:</b></label>
-                    <SecondaryButton onClick={() => {
-                        // Copy secret.toHexString() to clipboard
-                        navigator.clipboard.writeText(secretToSharedKey(secret));
-                        setEnableSharedCopy(false);
-                        setTimeout(() => {
-                            setEnableSharedCopy(true);
-                        }, 1000);
-                    }} disabled={!enableSharedCopy}>
-                        {enableSharedCopy ? "Copy" : "Copied!"}
-                    </SecondaryButton>
-                    <   span className={"px-1 bg-zinc-100 text-zinc-900 rounded-md font-mono"}>
-                	{secretToSharedKey(secret).substr(0, 6) + "..."}
-            	</span>
-                    <label><b>Status:</b></label>
-                    <SecondaryButton onClick={refresh} disabled={refreshing}>
-                        Refresh
-                    </SecondaryButton>
-                    <   span className={"px-1 bg-zinc-100 text-zinc-900 rounded-md font-mono"}>
-                	Unpaid
-            	</span>
-                </>
-            }
-        </div>
-    )
-}
+import {SecretDisplay} from "./components/SecretDisplay";
 
 function YourAssetsSection() {
     const {keys, assets} = useContext(SecretContext);
@@ -137,8 +35,12 @@ function YourAssetsSection() {
                                             <div key={idx} className={"bg-stone-800 p-2 rounded-lg"}>
                                                 <div className="flex flex-row justify-between">
                                                     {/*<span className={"text-white"}></span>*/}
-                                                    <SecretDisplay secret={secret} idx={idx} contract={contract}
-                                                                   isAsset={true}/>
+                                                    <SecretDisplay
+                                                        secret={secret}
+                                                        idx={idx}
+                                                        contract={contract}
+                                                        isAsset={true}
+                                                    />
                                                 </div>
                                             </div>
                                         )
